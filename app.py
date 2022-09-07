08359db40098063ef8c18d227c87b82ff593103c
Karlis <karlisstekels.dev@gmail.com>
2022-09-07 14:30:41 +0300

    Change the code for postgres heroku

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from security import authenticate, identity
 from flask import Flask
 from flask_jwt import JWT
@@ -10,7 +12,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
